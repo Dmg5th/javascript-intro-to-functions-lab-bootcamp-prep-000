ada2fbde016eba7c941b61b38b2d9628c754578f
Daniel Gould <dmgould310@gmail.com>
2020-06-08 19:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-if (string.toLowerCase()) === string {
+if (string.toLowerCase() === string) {
   return ("I can't hear you!")
 }
-if 
+if (string.toUpperCase() === string) {
+  return ("YES INDEED!")
+}
+if (string === "I love you")
 }
